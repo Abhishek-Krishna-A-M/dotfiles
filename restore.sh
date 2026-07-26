263eeb90725d830de8e65c81c10c845c73fb51ec
Abhishek Krishna A M <abhishekkrishna2k6@gmail.com>
2026-07-26 14:12:51 +0530

    Remove unwanted files

--- restore.sh
+++ restore.sh
@@ -1,3 +1,6 @@
+echo "==> Linking configuration files..."
+mkdir -p "$CONFIG_DIR"
+
 CONFIGS=(
   alacritty astro bspwm btop cava custom_scripts dconf dunst fish 
   flameshot foot fuzzel gh gtk-3.0 gtk-4.0 helix htop lazygit lf 
@@ -7,6 +10,9 @@ CONFIGS=(
 )
 
 for item in "${CONFIGS[@]}"; do
-  rm -rf "$HOME/.config/$item"
-  ln -s "$HOME/dotfiles/config/$item" "$HOME/.config/$item"
+  rm -rf "$CONFIG_DIR/$item"
+  ln -s "$DOTFILES_DIR/config/$item" "$CONFIG_DIR/$item"
+  echo "Linked: $item"
 done
+
+echo "==> Setup complete!"
